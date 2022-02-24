cd59a25094cb730da2358bb222df46c6d204d270
msynn <fyanramadhan1@gmail.com>
2022-02-24 23:33:59 +0930

    Method 2

--- BelajarOOP.py
+++ BelajarOOP.py
@@ -10,10 +10,22 @@ class User:
     def info(self):
         return f"{self.name} - {self.role} - {self.damage}"
 
+    def update_role(self, new_role = "fighter"):
+        if self.role == "assassin": # Jika role anda adalah assassin, maka anda bisa berubah role fighter.
+        # role selain assasin tidak bisa berubah.    
+            self.role = new_role
+        
 # instance variable VS class variable
 
 zilong = User("zilong", 100, "assassin")
 aurora = User("aurora", 200, "mage")
 argus = User("argus", 300, "warrior")
 
+
 print(zilong.info())
+zilong.update_role()
+print(zilong.info())
+
+print(aurora.info())
+aurora.update_role("assassin")
+print(aurora.info())
