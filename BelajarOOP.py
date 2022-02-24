34578920d0513d0c844c2dd09b3e0f9fc3bd9b94
msynn <fyanramadhan1@gmail.com>
2022-02-24 23:16:28 +0930

    method 1

--- BelajarOOP.py
+++ BelajarOOP.py
@@ -6,14 +6,14 @@ class User:
         self.damage = damage   # 100, 200, 300, etc.
         self.role = role   # assassin, mage, warrior, etc.
         User.total += 1
-        
+
+    def info(self):
+        return f"{self.name} - {self.role} - {self.damage}"
+
 # instance variable VS class variable
 
 zilong = User("zilong", 100, "assassin")
-print(User.total)
-
 aurora = User("aurora", 200, "mage")
-print(User.total)
-
 argus = User("argus", 300, "warrior")
-print(User.total)
+
+print(zilong.info())
