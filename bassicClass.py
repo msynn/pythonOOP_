1b7e2da954debbf30ab62fab9407a4ff400015c1
msynn <fyanramadhan1@gmail.com>
2022-02-24 22:19:11 +0930

    instance VS class variable

--- bassicClass.py
+++ bassicClass.py
@@ -1,13 +1,19 @@
 class User:
+    total = 0
+
     def __init__(self, name, damage, role):
-        self.name = name   # zilong, aurora
-        self.damage = damage   # 100, 200
-        self.role = role   # assassin, mage, warrior
-# name, role, damage
+        self.name = name   # zilong, aurora, argus, etc.
+        self.damage = damage   # 100, 200, 300, etc.
+        self.role = role   # assassin, mage, warrior, etc.
+        User.total += 1
+        
+# instance variable VS class variable
 
 zilong = User("zilong", 100, "assassin")
-aurora = User("aurora", 200, "mage")
+print(User.total)
 
-print(zilong.name)
-print(zilong.damage)
-print(zilong.role)
+aurora = User("aurora", 200, "mage")
+print(User.total)
+
+argus = User("argus", 300, "warrior")
+print(User.total)
