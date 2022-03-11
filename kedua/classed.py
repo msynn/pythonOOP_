34d8088ffcd262b351980be5f8202d11e1e37867
msynn <fyanramadhan1@gmail.com>
2022-03-11 23:48:35 +0930

    repisi

---
+++ kedua/classed.py
@@ -0,0 +1,12 @@
+class Item:
+    pass
+
+item1 = Item()
+item1.name = "Phone"
+item1.price = 100
+item1.quantity = 5
+
+print(type(item1))
+print(type(item1.name))
+print(type(item1.price))
+print(type(item1.quantity))
