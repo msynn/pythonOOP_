938dd058ebbdfa325ce802266dd7ce331ca8ba51
msynn <fyanramadhan1@gmail.com>
2022-03-14 22:21:03 +0930

    barang

--- kedua/constructor __init__.py
+++ kedua/constructor __init__.py
@@ -1,4 +1,6 @@
 class Item:
+    pay_rate = 0.8 # the pay rate after 20% discount
+    all = []
     def __init__(self, name: str , price: float, quantity=0):
         # Run validations to the received argument
         assert price >= 0, f"Price {price} is not greated than or equal to zero!"
@@ -10,12 +12,24 @@ class Item:
         self.price = price
         self.quantity = quantity
         
+        # Action to execute
+        Item.all.append(self)
+        
         
     def calculate_total_price(self):
         return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+    
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+        
 
 item1 = Item("Phone", 200, 9)
-
 item2 = Item("Laptop", 2000, 3)
+item3 = Item("Mouse", 50, 10)
+item4 = Item("Headset", 80, 4)
+item5 = Item("Bag", 120, 8)
 
-print(item1.calculate_total_price())
+print(Item.all)
